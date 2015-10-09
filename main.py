1784e0b1c170d8badf76e075d16c3361a9fa478f
Yu Wang <phoebe996@users.noreply.github.com>
2015-10-09 16:48:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,34 @@ import parser
 import interface
 
 def main(argv):
+    # Get parameters from user input
     accountKey = argv[0]
     target_precision = float(argv[1])
     query = argv[2].lower().split()
     precision = 0.0
+    # create a new search engine instance
     search_engine = search.SearchEngine(accountKey)
     while True:  
+        # search this query on Bing 
         xml = search_engine.search(query)
+        # print out search query parameters on interminal
         interface.print_parameters(search_engine, target_precision)
+        # parse the xml search results to Siteinfo objects
         result = parser.parse_xml(xml)
+        # if there are less than 10 results, break and quit
         if len(result) < 10:
             break
+        # interact with user and let user mark relevence of each result
         array_score, precision = interface.interact(result)
+        # if precision is 0, break and quit
         if precision == 0.0:
             print 'Below desired precision, but can no longer augment the query'
             break
         interface.print_transcript(precision, query, target_precision)
+        # if precision is equal or larger than target precision, break and quit
         if precision >= target_precision:
             break
+        # if precision is less than target precision, expand and reoder query and search again
         else:
             new_terms, new_query = search.query_expand(result, array_score, query)
             interface.print_new_terms(new_terms)
